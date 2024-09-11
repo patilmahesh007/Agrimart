5b21bc3a36606bb2842ae020afbe4169c873d0d0
22piyush <piyushaglawe05@gmail.com>
2024-09-11 17:32:07 +0530

    second component add new cards

--- src/config/BlogData.js
+++ src/config/BlogData.js
@@ -1,9 +1,14 @@
+
 import img1 from "../images/blogImages/img_1.jpg";
 import img2 from "../images/blogImages/img_2.jpg";
 import img3 from "../images/blogImages/img_3.jpg";
 import img4 from "../images/blogImages/img_4.jpg";
 import img5 from "../images/blogImages/img_5.jpg";
 import img6 from "../images/blogImages/img_6.jpg";
+import img7 from "../images/blogImages/7.jpg";
+import img8 from "../images/blogImages/8.jpg";
+import img9 from "../images/blogImages/9.jpg";
+
 
 const BlogHeading = "Latest Blogs";
 const TagLine =
@@ -163,6 +168,93 @@ const Blogs = [
   {
     id: 6,
     imgUrl: img6,
+    title: "First Blog",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit quis delectus modi possimus.",
+    content: [
+      "Modern farming methods have often proven effective and have delivered outstanding results. A farmer from Nashik, stands testimony to the wonders modern farming techniques can do. Here’s how Mr. Balu Darade grew a whopping 195 quintal onion –",
+
+      "NABARD’s golden initiative",
+
+      "The National Bank for Agriculture and Rural Development (NABARD) initiated a noble program where it trained the farmers for modern farming techniques. Mr. Balu Darade participated in this program as he wished to yield more than 100 quintal, his average produce.",
+
+      "Goodbye to traditional techniques",
+
+      "The traditional method of sowing on flat beds was replaced by raised beds. Following are the advantages of raised bed sowing –",
+
+      "Water can be supplied through drip irrigation, The temperature can be maintained in raised bed sowing, Spacing can be easily maintained Drip irrigation Raised bed sowing allowed the water to be supplied through drip irrigation, a method which has its own advantages –",
+
+      "Drip irrigation reduces water wastage Evaporation is minimal and the consumption is lowered Fertilisers can be used via the dripping system The land between the plant rows remain dry and unwanted plant growth is prevented",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi! Fuga sed doloremque laborum modi voluptatem commodi voluptates. Cumque esse nulla error in a, saepe debitis at totam molestias ad!",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi! Fuga sed doloremque laborum modi voluptatem commodi voluptates. Cumque esse nulla error in a, saepe debitis at totam molestias ad!",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi!",
+    ],
+    details:  "oct 23,2019 | Piyush Aglawe",
+  },
+  {
+    id: 7,
+    imgUrl: img7,
+    title: "First Blog",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit quis delectus modi possimus.",
+    content: [
+      "Modern farming methods have often proven effective and have delivered outstanding results. A farmer from Nashik, stands testimony to the wonders modern farming techniques can do. Here’s how Mr. Balu Darade grew a whopping 195 quintal onion –",
+
+      "NABARD’s golden initiative",
+
+      "The National Bank for Agriculture and Rural Development (NABARD) initiated a noble program where it trained the farmers for modern farming techniques. Mr. Balu Darade participated in this program as he wished to yield more than 100 quintal, his average produce.",
+
+      "Goodbye to traditional techniques",
+
+      "The traditional method of sowing on flat beds was replaced by raised beds. Following are the advantages of raised bed sowing –",
+
+      "Water can be supplied through drip irrigation, The temperature can be maintained in raised bed sowing, Spacing can be easily maintained Drip irrigation Raised bed sowing allowed the water to be supplied through drip irrigation, a method which has its own advantages –",
+
+      "Drip irrigation reduces water wastage Evaporation is minimal and the consumption is lowered Fertilisers can be used via the dripping system The land between the plant rows remain dry and unwanted plant growth is prevented",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi! Fuga sed doloremque laborum modi voluptatem commodi voluptates. Cumque esse nulla error in a, saepe debitis at totam molestias ad!",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi! Fuga sed doloremque laborum modi voluptatem commodi voluptates. Cumque esse nulla error in a, saepe debitis at totam molestias ad!",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi!",
+    ],
+    details:  "oct 23,2019 | Piyush Aglawe",
+  },
+  {
+    id: 8,
+    imgUrl: img8,
+    title: "First Blog",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit quis delectus modi possimus.",
+    content: [
+      "Modern farming methods have often proven effective and have delivered outstanding results. A farmer from Nashik, stands testimony to the wonders modern farming techniques can do. Here’s how Mr. Balu Darade grew a whopping 195 quintal onion –",
+
+      "NABARD’s golden initiative",
+
+      "The National Bank for Agriculture and Rural Development (NABARD) initiated a noble program where it trained the farmers for modern farming techniques. Mr. Balu Darade participated in this program as he wished to yield more than 100 quintal, his average produce.",
+
+      "Goodbye to traditional techniques",
+
+      "The traditional method of sowing on flat beds was replaced by raised beds. Following are the advantages of raised bed sowing –",
+
+      "Water can be supplied through drip irrigation, The temperature can be maintained in raised bed sowing, Spacing can be easily maintained Drip irrigation Raised bed sowing allowed the water to be supplied through drip irrigation, a method which has its own advantages –",
+
+      "Drip irrigation reduces water wastage Evaporation is minimal and the consumption is lowered Fertilisers can be used via the dripping system The land between the plant rows remain dry and unwanted plant growth is prevented",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi! Fuga sed doloremque laborum modi voluptatem commodi voluptates. Cumque esse nulla error in a, saepe debitis at totam molestias ad!",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi! Fuga sed doloremque laborum modi voluptatem commodi voluptates. Cumque esse nulla error in a, saepe debitis at totam molestias ad!",
+
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eaque, esse voluptatem eveniet at culpa officia id veritatis dolorem ab dolorum quas distinctio iste sint assumenda illum corporis saepe excepturi!",
+    ],
+    details:  "oct 23,2019 | Piyush Aglawe",
+  },
+  {
+    id: 9,
+    imgUrl: img9,
     title: "First Blog",
     description:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit quis delectus modi possimus.",
