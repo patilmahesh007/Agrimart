c9cf74dd550f8a63dcd1f8ec18363a171da45007
Prathamesh Deogirkar <prathameshdeogirkar22485@gmail.com>
2024-09-12 23:55:16 +0530

    onclick events added

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './TopBar.css';
 
-
-import Dairy from './../../view/dairy/Dairy';
-import Essentials from './../../view/essentials/Essentials';
-import Fruits from './../../view/fruits/Fruits';
-import Exotics from './../../view/exotics/Exotics';
-import Vegetables from './../../view/vegetables/Vegetables';
-
-const TopBar = () => {
+const TopBar = ({ setSelectedCategory }) => {
   return (
     <>
-    
       <div className="top-bar">
         <div className="left">
           <span>LOG IN </span>
@@ -26,14 +17,12 @@ const TopBar = () => {
                 alt='facebook icon'
                 className='social-media-icon' />
             </a>
-
             <a href='https://www.instagram.com/' target='blank'>
               <img src='https://cdn-icons-png.flaticon.com/128/6422/6422200.png'
                 alt='instagram icon'
                 className='social-media-icon' />
             </a>
           </div>
-
           <span className='price'>RS. 0.00</span>
           <span>
             <img src='https://cdn-icons-png.flaticon.com/128/3916/3916624.png'
@@ -44,14 +33,12 @@ const TopBar = () => {
       </div>
 
       <nav className="menu top-bar">
-        <Link to="/Essentials">ESSENTIALS</Link>
-        <Link to="/Vegetables">VEGETABLES</Link>
-        <Link to="/Fruits">FRUITS</Link>
-        <Link to="/Exotics">EXOTICS</Link>
-        <Link to="/Dairy">DAIRY</Link>
+        <span onClick={() => setSelectedCategory('Essentials')}>ESSENTIALS</span>
+        <span onClick={() => setSelectedCategory('Vegetables')}>VEGETABLES</span>
+        <span onClick={() => setSelectedCategory('Fruits')}>FRUITS</span>
+        <span onClick={() => setSelectedCategory('Exotics')}>EXOTICS</span>
+        <span onClick={() => setSelectedCategory('Dairy')}>DAIRY</span>
       </nav>
-
-
     </>
   );
 };
