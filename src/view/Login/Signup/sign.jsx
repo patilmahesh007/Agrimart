36eb3ab4332beb210f53d80676d7a564c36984db
Rutuja20Sanap <dnyaneshwarsanap293@gmail.com>
2024-09-13 00:33:46 -0930

    added username input field

--- src/view/Login/Signup/sign.jsx
+++ src/view/Login/Signup/sign.jsx
@@ -4,18 +4,49 @@ import Navbar from "../../../components/navbar/nav";
 import IMG from "./img/bg.svg";
 
 
-function sign() {
+const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+  });
+
   return (
-    <div>
+    <>
         <div><Navbar/></div>
         <div className="signup-container">
         <div className="img">
           <img src={IMG} alt="Background" />
         </div>
+        <div className="login-content">
+          <form onSubmit={handleSubmit}/>
+            <img src={img} alt="Avatar" />
+            <h3 className="title">Create Account</h3>
+
+            <div className="input-div one">
+              <div className="i">
+                <i className="fas fa-user"></i>
+              </div>
+              <div className="div">
+                <h5>Username</h5>
+                <input
+                  type="text"
+                  className="input"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
         </div>
-        <Footer/>
-    </div>
+        </div>
+        <div><Footer/></div>
+        
+    </>
   )
 }
 
-export default sign
+export default SignupForm 
