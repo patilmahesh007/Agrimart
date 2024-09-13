91e55459ff741b9c5bccfaba2bad45f8f4919c45
Rutuja20Sanap <dnyaneshwarsanap293@gmail.com>
2024-09-13 01:13:57 -0930

    making function for form handling

--- src/view/Login/login.jsx
+++ src/view/Login/login.jsx
@@ -2,12 +2,63 @@ import React, { useState } from 'react';
 import toast, { Toaster } from "react-hot-toast";
 import Footer from "../../components/footer/footer";
 import Navbar from "../../components/navbar/nav";
+import IMG from "../img/bg.svg";
+import img from "../img/avatar.svg";
+import { Link } from "react-router-dom";
+import "./login.css";
 
-function login() {
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+ 
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+   
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+
+ 
+    if (storedUsername && storedPassword) {
+      if (formData.username === storedUsername && formData.password === storedPassword) {
+        toast.success('Login successful!');
+       
+      } else {
+        toast.error('Invalid username or password!');
+      }
+    } else {
+      toast.error('No account found. Please sign up first.');
+    }
+  };
+
+
+  const handleForgotPassword = (e) => {
+    e.preventDefault(); 
+
+ 
+    const storedEmail = localStorage.getItem('email');
+    if (storedEmail) {
+      toast.info(`Password reset link sent to: ${storedEmail}`);
+    } else {
+      toast.error('No email found. Please sign up first.');
+    }
+  };
+
   return (
     <div>
         <div><Navbar/></div>
-        <div className="signup-container">
+        <div className="login-container">
 
         </div>
         <Footer/>
@@ -15,4 +66,4 @@ function login() {
   )
 }
 
-export default login
+export default LoginForm
