813cb52d5867cbeb1e41ad9003b52106f73c80dd
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2024-09-14 16:45:56 +0530

    Essentials card add

---
+++ src/components/Exoticscard/Exoticscard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../Frutecard/Frutecard.css'
+
+function Exoticscard({title,img,prise}) {
+  return (
+    
+    
+    <div className='veg-continer-bg'>
+      <div className=' frute-img-bg'>
+      <img src={img} className='veg-img-bg'/></div>
+      <div className='info'>
+      <h3>{title}</h3>
+      
+      
+      <b> Price: {prise}</b> </div>
+      <div className='btn-continer'>
+      <button className='card-btn'> More</button>
+      </div>
+    </div>
+  )
+}
+
+export default Exoticscard
--- src/view/exotics/Exotics.js
+++ src/view/exotics/Exotics.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import Exoticscard from '../../components/Exoticscard/Exoticscard'
+import { ExoticsDATA } from '../../config/Exoticsdata'
 
 function Exotics() {
   return (
-    <div>Exotics</div>
+    <div className='veg-card-continer'>
+    {
+      ExoticsDATA.map((product)=>{
+        
+       return(
+        <Exoticscard
+        
+        img ={product.photo}
+        title={product.name}
+        prise={product.price}
+
+        
+        />
+
+       )
+
+
+        
+      })
+    }
+
+</div>
   )
 }
 
