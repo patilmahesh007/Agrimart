9633dcc35d49c8a8b1e60e7683857213859cf3e6
Prathamesh Deogirkar <prathameshdeogirkar22485@gmail.com>
2024-09-17 02:11:05 +0530

    toggle added

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,12 +1,13 @@
 import './nav.css';
-import { NavLink, useLocation } from 'react-router-dom'; // Importing useLocation to detect route changes
-import React, { useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import { headerdata } from './../../config/config';
 import { Toaster, toast } from 'react-hot-toast';
 import brandIcon from "./../../../src/images/nav/logo2.png";
 
 function Header() {
   const location = useLocation();
+  const [menuActive, setMenuActive] = useState(false);
 
   useEffect(() => {
 
@@ -45,8 +46,11 @@ function Header() {
       });
     }
 
-
   }, [location]);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
 
   return (
     <div className='header-container'>
@@ -58,7 +62,8 @@ function Header() {
             </NavLink>
           </div>
 
-          <ul className="nav-menu">
+          <ul className={`nav-menu ${menuActive ? 'active' : ''}`} >
+
             {headerdata.map((item, index) => (
               <li className="nav-item" key={index}>
                 <NavLink
@@ -73,6 +78,12 @@ function Header() {
               </li>
             ))}
           </ul>
+
+          <div className="dropdown-items" onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </nav>
       </header>
 
