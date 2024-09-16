e07795c0b077a79d68c6b0b5a01fc43df34f5a73
patilmahesh007 <patilmahesh4007>
2024-09-16 14:13:11 +0530

    milk card data addeds

--- src/components/Frutecard/Frutecard.js
+++ src/components/Frutecard/Frutecard.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cardData from '../../config/shopCardData';
 import { Link } from 'react-router-dom';
 import './Frutecard.css';
 
-function FruitCard() {
+function FruitCard() { 
+  const [Quantity, setQuantity] = useState(0);
   return (
+    <>
     <div className="App-fruit">
       {cardData.map((card) => {
         return (
@@ -17,12 +19,16 @@ function FruitCard() {
                 <h3>{card.title}</h3>
                 <p>Price: {card.price}</p>
                 <del> {card.oldPrice}</del>
+
               </div>
             </div>
           </Link>
         );
       })}
+              <div className='Buy-now-btn'> <img src='https://cdn-icons-png.flaticon.com/128/149/149163.png' onClick={() => setQuantity(Quantity + 1)} />Buy Now <img onClick={() => setQuantity(Quantity - 1)} src='https://cdn-icons-png.flaticon.com/128/3524/3524384.png'/></div>
+
     </div>
+    </>
   );
 }
 
--- src/config/DairyCardData.js
+++ src/config/DairyCardData.js
@@ -1,4 +1,4 @@
-import Apple from "./../images/fruitcard/apple.png";
+import Milk from "./../images/dairycard/milk.png";
 
 
 
@@ -8,7 +8,7 @@ const cardData = [
         id: "400",
         title: "Dairy",
         description: "A delicious, sweet fruit available in various colors.",
-        image: Apple,
+        image: Milk,
         price: "₹80/kg",
         oldPrice: "₹100/kg",
         quantity: "1 kg",
@@ -20,7 +20,7 @@ const cardData = [
         healthBenefits: "High in fiber, vitamin C, and antioxidants. Promotes heart health, supports weight management, and reduces the risk of diabetes.",
         storageTemperature: "0-4°C",
         storageType: "Refrigeration",
-        imageSlider: [Apple, "https://i.pinimg.com/236x/4f/6c/c4/4f6cc46e50e7a0ff21c5e0a77423b0b5.jpg", "https://i.pinimg.com/236x/fe/0d/1d/fe0d1d36996a0e72931d080983773c1e.jpg","https://i.pinimg.com/236x/62/a8/bd/62a8bd58fbc9bb5d7e5517ac0d2d1ce6.jpg"]
+        imageSlider: [Milk, "https://i.pinimg.com/236x/4c/7e/22/4c7e22ad99ab10035ffa370bff1978a1.jpg", "https://i.pinimg.com/236x/fe/0d/1d/fe0d1d36996a0e72931d080983773c1e.jpg","https://i.pinimg.com/236x/62/a8/bd/62a8bd58fbc9bb5d7e5517ac0d2d1ce6.jpg"]
     },
 
 ];
