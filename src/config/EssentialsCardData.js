037720722ce2358255fd610e88d61bc10754d646
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2024-09-16 16:29:59 +0530

    add update card

--- src/config/EssentialsCardData.js
+++ src/config/EssentialsCardData.js
@@ -1,4 +1,7 @@
 import Apple from "./../images/fruitcard/apple.png";
+import chill from "../../src/images/card/chill.png"
+
+import cucumber from "../../src/images/card/cucumber.png"
 
 
 
@@ -22,6 +25,53 @@ const EssentialsCardData = [
         storageType: "Refrigeration",
         imageSlider: [Apple, "https://i.pinimg.com/236x/4f/6c/c4/4f6cc46e50e7a0ff21c5e0a77423b0b5.jpg", "https://i.pinimg.com/236x/fe/0d/1d/fe0d1d36996a0e72931d080983773c1e.jpg","https://i.pinimg.com/236x/62/a8/bd/62a8bd58fbc9bb5d7e5517ac0d2d1ce6.jpg"]
     },
+    {
+        id: "102",
+        title: "Fresh Chili ",
+        description: "Vibrant and spicy chili peppers that bring heat and flavor to your meals.",
+        image: chill,
+        price: "₹90/kg",
+        oldPrice: "₹110/kg",
+        quantity: "1 kg",
+        shelfLife: "2 weeks",
+        farmer: "Kiran Patel",
+        manufacturer: "Spice Growers Co-op, Andhra Pradesh 530016. Lic. No. - 10024031001117.",
+        sellerFSSAI: "24345678000107",
+        marketedBy: "Fiery Flavors, Hyderabad 500001.",
+        healthBenefits: "High in vitamins A and C. Contains capsaicin, which may boost metabolism, reduce inflammation, and provide pain relief.",
+        storageTemperature: "10-15°C",
+        storageType: "Cool storage",
+        imageSlider: [chill,
+            "https://cdn.pixabay.com/photo/2023/12/09/21/03/chilli-pepper-8440176_1280.jpg",
+            "https://img.freepik.com/free-photo/red-chilli-paste-black_1150-25554.jpg?t=st=1726483389~exp=1726486989~hmac=fe0e9d4334bb038bddc634a91e167c412c8ce8952a10d10faaac44b72596d994&w=740",
+            "https://img.freepik.com/free-photo/red-chilli-paste-black_1150-25548.jpg?uid=R163383201&ga=GA1.1.1649115650.1725956968&semt=ais_hybrid"
+        ]
+    },
+    {
+        id: "103",
+        title: "Fresh Cucumbers",
+        description: "Crisp and refreshing cucumbers, perfect for salads, snacks, or adding a cool crunch to your dishes.",
+        image: cucumber,
+        price: "₹60/kg",
+        oldPrice: "₹75/kg",
+        quantity: "1 kg",
+        shelfLife: "1 week",
+        farmer: "Sita Rani",
+        manufacturer: "Green Fields Agriculture, Punjab 140001. Lic. No. - 10024031001121.",
+        sellerFSSAI: "24345678000111",
+        marketedBy: "Cool Veggies, Chennai 600001.",
+        healthBenefits: "High in water content and low in calories. Rich in vitamins K and C, and has antioxidants. Helps in hydration and supports skin health.",
+        storageTemperature: "7-10°C",
+        storageType: "Refrigerated storage",
+        imageSlider: [
+            cucumber,
+            "https://img.freepik.com/free-photo/top-view-cucumber-brown-wooden-horizontal_176474-1267.jpg?uid=R163383201&ga=GA1.1.1649115650.1725956968&semt=ais_hybrid",
+            "https://img.freepik.com/premium-photo/falling-cucumber-slice-isolated-white-background_1153035-6835.jpg?uid=R163383201&ga=GA1.1.1649115650.1725956968&semt=ais_hybridhttps://img.freepik.com/premium-photo/falling-cucumber-slice-isolated-white-background_1153035-6835.jpg?uid=R163383201&ga=GA1.1.1649115650.1725956968&semt=ais_hybrid",
+            "https://img.freepik.com/premium-photo/close-up-green-beans-against-white-background_1048944-6171364.jpg?uid=R163383201&ga=GA1.1.1649115650.1725956968&semt=ais_hybrid"
+        ]
+    }
+    
+    
 
 ];
 export default EssentialsCardData
