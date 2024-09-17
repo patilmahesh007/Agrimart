adbc084fef8a7703ffe1c29418b0523db5557e61
patilmahesh007 <patilmahesh4007>
2024-09-17 23:56:59 +0530

    cart error fixed

--- src/components/CartSection/CartSection.js
+++ src/components/CartSection/CartSection.js
@@ -20,21 +20,10 @@ const TopBarSection = () => {
       </div>
 
       <div className="right">
-        <div className="social-icons">
-          <a href='https://www.facebook.com' target='_blank' rel='noreferrer'>
-            <img src='https://cdn-icons-png.flaticon.com/128/6422/6422199.png'
-              alt='facebook icon'
-              className='social-media-icon' />
-          </a>
-          <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
-            <img src='https://cdn-icons-png.flaticon.com/128/6422/6422200.png'
-              alt='instagram icon'
-              className='social-media-icon' />
-          </a>
-        </div>
+        
         <span className='price'>RS. 0.00</span>
 
-        <span onClick={() => setSelectedCategory('Cart')}>
+        <span onClick={() => setIsCartOpen(!isCartOpen)}>
           <img src='https://cdn-icons-png.flaticon.com/128/3916/3916624.png'
             alt='cart icon'
             className='social-media-icon' />
