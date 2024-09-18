a462a6f5df212e8b7a274952abbea4d45d1f2965
Tusharzade <tusharzade29@gmail.com>
2024-09-18 20:27:42 +0530

    input box adding

--- src/view/Checkout/Checkout.js
+++ src/view/Checkout/Checkout.js
@@ -1,15 +1,24 @@
 import React from 'react'
-
+import './Checkout.css'
 function Checkout() {
   return (
     <div>
       <div>
-      <h1>Contact</h1>
-        <input type='text' />
-        <h3>  Enter an email</h3>
+
+        <hr/>
       </div>
-      <div>
-             
+      <div className='checkout-leftside'>
+        <h1 className='contact-heading'>Contact</h1>
+        <input type='text' className='email-input' placeholder='enter a email'/>
+        <h1 className='contact-heading' >Delivery</h1>
+        <input type='text' className='country-input' placeholder='country'/>
+        <div className='fullname-container'>
+           <input type='text' className='firstname-input' placeholder='country'/>
+           <input type='text' className='lastname-input' placeholder='country'/>
+        </div>
+      </div>
+      <div className='checkout-rightside'>
+              
       </div>
     </div>
   )
