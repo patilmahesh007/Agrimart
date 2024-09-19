bab640a5bef8e8d98179d903c554d099396101c3
Tusharzade <tusharzade29@gmail.com>
2024-09-19 18:59:12 +0530

    complete all inputs

--- src/view/Checkout/Checkout.js
+++ src/view/Checkout/Checkout.js
@@ -7,6 +7,7 @@ function Checkout() {
 
         <hr/>
       </div>
+    <div className='main-container'/>
       <div className='checkout-leftside'>
         <h1 className='contact-heading'>Contact</h1>
         <input type='text' className='checkout-email-input' style={{ width:500 }}placeholder='enter a email'/>
@@ -16,19 +17,23 @@ function Checkout() {
            <input type='text' className='firstname-input' style={{ width:200 }} placeholder='Firstname'/>
            <input type='text' className='lastname-input'style={{ width:200 }} placeholder='Lastname'/>
         </div>
-        <input type='text' className='Address-input'  style={{ width:500 }} placeholder='Address'/>
+        <input type='text' className='Address-input'  style={{ width:500 }} placeholder='Address'/> <br></br>
         <input type='text' className='Apartment-input' style={{ width:500 }}  placeholder='Apartment'/>
 
         <div className='input-container-address'>
-             <input type='text' className='city-input' placeholder='City'/>  
-             <input type='text' className='state-input' placeholder='State'/>  
-             <input type='text' className='pincode-input' placeholder='Pin code'/>
+             <input type='text' className='inputs-all city-input' style={{ width:100 }}  placeholder='City'/>  
+             <input type='text' className='inputs-all state-input' style={{ width:100 }}  placeholder='State'/>  
+             <input type='text' className='inputs-all pincode-input' style={{ width:100 }}  placeholder='Pin code'/>
         </div>
-        <input type='text' className='phonenumber-input' placeholder='Phone Number'/>
+        <input type='text' className='phonenumber-input' style={{ width:500 }}  placeholder='Phone Number'/>
       </div>
+    
+    
       <div className='checkout-rightside'>
-              
+               
       </div>
+        
+
     </div>
   )
 }
