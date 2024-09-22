791c00792f2cd6f934db1bc73e076de2646defe6
karishmawadaskar <karishmawadaskar@gmail.com>
2024-09-22 16:12:47 +0530

    make a bill page responsive

--- src/view/Bill/Bill.jsx
+++ src/view/Bill/Bill.jsx
@@ -75,71 +75,78 @@ function Bill() {
             <div className='bill-page'>
               {/* Bill Details */}
               <div className='bill-page-div1'>
-                <h1>Agrimart</h1>
-                <p>Hadapsar, Pune, Maharashtra, 411028</p>
-                <p><b>Phone:</b> +91 8468954007</p>
-                <p><b>GSTIN:</b> 0AHST7UMKS6REKSY</p>
-                <p><b>PAN Number:</b> GOPPP5638E</p>
-
+                <div className='bill-page-div1-d1'>
+                  <h1>Agrimart</h1>
+                  <p>Hadapsar, Pune, Maharashtra, 411028</p>
+                  <p><b>Phone:</b> +91 8468954007</p>
+                  <p><b>GSTIN:</b> 0AHST7UMKS6REKSY</p>
+                  <p><b>PAN Number:</b> GOPPP5638E</p>
+                </div>
                 {/* Customer Info */}
-                <p className='bill-page-div1-line'>BILL TO</p>
-                <h3>{customerData.firstName} {customerData.lastName}</h3>
-                <p>{customerData.apartment}, {customerData.address}, {customerData.city}, {customerData.state}</p>
-                <p><b>Pin:</b> {customerData.zipCode}</p>
-                <p><b>Phone:</b> {customerData.phoneNumber}</p>
-                <p><b>Place of Supply:</b> {customerData.state}</p>
-
+                <div className='bill-page-div1-d2'>
+                  <p className='bill-page-div1-line'>BILL TO</p>
+                  <h3>{customerData.firstName} {customerData.lastName}</h3>
+                  <p>{customerData.apartment}, {customerData.address}, {customerData.city}, {customerData.state}</p>
+                  <p><b>Pin:</b> {customerData.zipCode}</p>
+                  <p><b>Phone:</b> {customerData.phoneNumber}</p>
+                  <p><b>Place of Supply:</b> {customerData.state}</p>
+                </div>
                 {/* Purchased Items */}
-                <p className='bill-page-div1-line2'><span>Items Purchased</span> <span> Items Quantity </span></p>
-                <div className='dummy-'>
-                  <div>
-                    {Object.values(billdata).map((item) => (
-                      <div key={item.key}>
-                        <p>{item.name}</p>
-                      </div>
-                    ))}
-                  </div>
-                  <div>
-                    {Object.values(billdata).map((item) => (
-                      <div key={item.key}>
-                        <p>{item.quantity}</p>
-                      </div>
-                    ))}
+                <div>
+                  <p className='bill-page-div1-line2'><span>Items Purchased</span> <span> Items Quantity </span></p>
+                  <div className='dummy-'>
+                    <div>
+                      {Object.values(billdata).map((item) => (
+                        <div key={item.key}>
+                          <p>{item.name}</p>
+                        </div>
+                      ))}
+                    </div>
+                    <div>
+                      {Object.values(billdata).map((item) => (
+                        <div key={item.key}>
+                          <p>{item.quantity}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
-
                 {/* Bank Details */}
-                <p className='bill-page-div1-line'>Bank Details</p>
-                <b>Bank Details</b>
-                <p>Account holder: Patil Mahesh</p>
-                <p>Account number: 34210538662</p>
-                <p>Bank: SBI</p>
-                <p>Branch: Satral SAB</p>
-                <p>IFSC: SBIN00089471</p>
-                <p>UPI ID: 1212@rzrpay</p>
-                <br />
-                <b>Note</b>
-                <p>1. No Return Policy</p>
-                <br />
-                <b>Terms & Conditions</b>
-                <p>1. Customer will pay the taxes</p>
-                <p>2. Customer will pay the delivery charges</p>
-                <p>3. No Cash on Delivery</p>
+                <div>
+                  <p className='bill-page-div1-line'>Bank Details</p>
+                  <b>Bank Details</b>
+                  <p>Account holder: Patil Mahesh</p>
+                  <p>Account number: 34210538662</p>
+                  <p>Bank: SBI</p>
+                  <p>Branch: Satral SAB</p>
+                  <p>IFSC: SBIN00089471</p>
+                  <p>UPI ID: 1212@rzrpay</p>
+                  <br />
+                  <b>Note</b>
+                  <p>1. No Return Policy</p>
+                  <br />
+                  <b>Terms & Conditions</b>
+                  <p>1. Customer will pay the taxes</p>
+                  <p>2. Customer will pay the delivery charges</p>
+                  <p>3. No Cash on Delivery</p>
+                </div>
               </div>
-
               <hr />
 
               {/* Tax Invoice */}
               <div className='bill-page-div2'>
-                <h1>TAX INVOICE</h1>
-                <p><b>Invoice Number:</b> 1234567890</p>
-                <p><b>Invoice Date:</b> {getcurrentdate}</p>
-
+                <div className='bill-page-div1-d1'>
+                  <h1>TAX INVOICE</h1>
+                  <p><b>Invoice Number:</b> 1234567890</p>
+                  <p><b>Invoice Date:</b> {getcurrentdate}</p>
+                </div>
                 {/* Shipping Info */}
-                <p className='bill-page-div2-line'>SHIP TO</p>
-                <h3>{customerData.firstName} {customerData.lastName}</h3>
-                <p>{customerData.apartment}, {customerData.address},<br />{customerData.city}, {customerData.state}, {customerData.zipCode}</p>
+                <div className='bill-page-div1-d2'>
+                  <p className='bill-page-div2-line'>SHIP TO</p>
 
+                  <h3>{customerData.firstName} {customerData.lastName}</h3>
+                  <p>{customerData.apartment}, {customerData.address},<br />{customerData.city}, {customerData.state}, {customerData.zipCode}</p>
+                </div>
                 {/* Price Details */}
                 <p className='bill-page-div2-line2'><span>Price per unit</span> <span>Product Total Amount</span></p>
                 <div className='dummy-'>
@@ -168,19 +175,21 @@ function Bill() {
                 </div>
 
                 <p className='bill-page-div1-line'>BILL TO</p>
+
                 <img className='stamp-image' src={stamp} alt='Stamp' />
 
                 <div className='total-ammount-div'>
                   <p>Total amount: ₹{totalPrice}</p>
                   <hr />
                   <p>Amount Paid: ₹{totalPrice}</p>
-                  <hr />
-                  <Toaster />
                 </div>
+                <hr />
+                <Toaster />
               </div>
             </div>
           </div>
         </div>
+
       ) : (
         <h1 className='checkout-redirect'>Redirecting to Login...</h1>
       )}
